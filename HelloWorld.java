13948458aff1be982a5643e9fd760d4d2915f022
Chen <Chen@0119.com>
2019-09-09 18:08:17 +0800

    created a HelloWorld class

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
-// HeeloWorld.java
-public class HelloWorld{
-    public static void main(String[] args){
-	     System.out.println("Hello World");
-		 }
+// HelloWorld.java
+public class HelloWorld {
+    public static void main(Strig[] arfs) {
+	     Systme.out.println("Hello World");
+	}
 }
